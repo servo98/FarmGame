00ef2fac0d9c620ab11514f6319170b7ca629711
servo pcerda <fernandonando98@live.com.mx>
2023-09-12 16:54:46 -0600

    feat: control update closes #14

--- src/Engine/game/Controls.ts
+++ src/Engine/game/Controls.ts
@@ -1,19 +1,50 @@
+import { KEYS, IsPressedType, JsonControl } from '../types/game/Control';
+import { file } from '../utils';
+
 export default class Control {
-  keysDown: Set<string>;
+  keys: Map<KEYS, IsPressedType>;
   constructor() {
-    this.keysDown = new Set<string>();
+    this.keys = new Map<KEYS, IsPressedType>();
   }
 
-  load() {
+  async load() {
+    const jsonControls = (await file.loadJsonFile(
+      'resources/CONFIG/controls.json',
+    )) as JsonControl;
+
+    for (const [key, value] of Object.entries(jsonControls)) {
+      this.keys.set(KEYS[key as keyof typeof KEYS], {
+        isPressed: false,
+        key: value,
+      });
+    }
+
     //load controlls
     document.addEventListener('keydown', (event) => {
-      //   console.log(`tecla ${event.key} abajo`);
-      this.keysDown.add(event.key);
+      const temporalKey = this.keyToKEY(event.key);
+      if (!temporalKey) return;
+      const temp = this.keys.get(temporalKey) as IsPressedType;
+      temp.isPressed = true;
+      this.keys.set(temporalKey, temp);
     });
 
     document.addEventListener('keyup', (event) => {
-      //   console.log(`tecla ${event.key} arriba`);
-      this.keysDown.delete(event.key);
+      const temporalKey = this.keyToKEY(event.key);
+      if (!temporalKey) return;
+      const temp = this.keys.get(temporalKey) as IsPressedType;
+      temp.isPressed = false;
+      this.keys.set(temporalKey, temp);
     });
+  }
+
+  private keyToKEY(searchKey: string): KEYS | null {
+    // console.log(searchKey);
+
+    for (const [key, isPressedObj] of this.keys) {
+      if (isPressedObj.key.toUpperCase() === searchKey.toUpperCase()) {
+        return key;
+      }
+    }
+    return null;
   }
 }
--- src/Engine/game/Player.ts
+++ src/Engine/game/Player.ts
@@ -3,6 +3,7 @@ import IPlayable from '../types/game/interfaces/IPlayable';
 import Entity from '../objet/Entity';
 import { PlayerArgsType } from '../types/game/Player';
 import { AnimationType } from '../types/object/AnimatedObject';
+import { KEYS } from '../types/game/Control';
 
 export default class Player extends Entity implements IPlayable {
   name: string;
@@ -13,23 +14,22 @@ export default class Player extends Entity implements IPlayable {
     this.name = args.name;
   }
 
-  input(controll: Control) {
-    if (controll.keysDown.has('s')) {
+  input(control: Control) {
+    if (control.keys.get(KEYS.DOWN)?.isPressed) {
       this.currentSpeed.y = this.maxSpeed;
       this.currentAnimation = this.animations.get('walk_down') as AnimationType;
-    } else if (controll.keysDown.has('w')) {
+    } else if (control.keys.get(KEYS.UP)?.isPressed) {
       this.currentSpeed.y = -this.maxSpeed;
       this.currentAnimation = this.animations.get('walk_up') as AnimationType;
     } else {
       this.currentSpeed.y = 0;
     }
-
-    if (controll.keysDown.has('d')) {
+    if (control.keys.get(KEYS.RIGHT)?.isPressed) {
       this.currentSpeed.x = this.maxSpeed;
       this.currentAnimation = this.animations.get(
         'walk_right',
       ) as AnimationType;
-    } else if (controll.keysDown.has('a')) {
+    } else if (control.keys.get(KEYS.LEFT)?.isPressed) {
       this.currentSpeed.x = -this.maxSpeed;
       this.currentAnimation = this.animations.get('walk_left') as AnimationType;
     } else {
